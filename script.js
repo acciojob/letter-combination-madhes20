88e13224af3285b8bdf0232740599751f63e62a1
madhes20 <118300068+madhes20@users.noreply.github.com>
2023-01-28 21:21:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function letterCombinations(input_digit) {
   //Complete the function
+const map = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+	  let result = [''];
+	  for (let digit of input_digit) {
+	      let letters = map[digit];
+	      let temp = [];
+	      for (let letter of letters) {
+	          for (let r of result) {
+	              temp.push(r + letter);
+	          }
+	      }
+	      console.log(temp)
+	      result = temp;
+		  
+	  }
+	result=result.sort();
+	return result;
 }
 
 module.exports = letterCombinations;
